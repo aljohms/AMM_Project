c472546e6a304b6f2511e3e9bb5533a5a536b844
MOHAMMED ALJOHANI <mohd919_3@hotmail.com>
2020-12-23 14:59:13 +0300

    Layout

--- AMM_Project.Frontend/wwwroot/js/site.js
+++ AMM_Project.Frontend/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("navbar").style.padding = "0.10rem 1rem";
+    } else {
+        document.getElementById("navbar").style.padding = "0.5rem 1rem";
+    }
+}
